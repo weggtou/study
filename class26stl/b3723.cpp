d276ed7a95474bb41a3a9750964306488f810627
weggt <3044129132@qq.com>
2025-09-10 17:08:03 +0800

    1

--- class26stl/b3723.cpp
+++ class26stl/b3723.cpp
@@ -1,6 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+
 int main(){
-    
+    unsigned long long F=0,B=0;    
+    multiset <unsigned long long> all;
+
+    int n;
+    cin >> n;
+    while(n--){
+        unsigned long long x;
+        cin >> x;
+        all.insert(x);
+    }
+    auto select = [&](unsigned long long &a){
+      auto i = all.upper_bound(a);
+      if (i!=all.begin())i--;
+      a+=*i;
+      all.erase(i);
+    };
+
+    while (!all.empty()){
+        select(F);
+        if (all.empty())break;
+        select(B);
+    }
+    cout << F << ' ' << B;
 }
