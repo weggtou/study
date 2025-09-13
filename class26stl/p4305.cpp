df2cede1b133d4918f544ba7cf93a19e59046aa4
weggt <3044129132@qq.com>
2025-09-13 19:42:57 +0800

    1

---
+++ class26stl/p4305.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+int main(){
+  int N;
+  cin >> N;
+  while(N--){
+    unordered_set <int> all;
+    vector <int> all2;
+    int n;
+    cin >> n;
+    while(n--){
+      int x;
+      cin >> x;
+      if(all.find(x)==all.end()){
+        all.insert(x);
+        all2.push_back(x);
+      }
+    }
+    for(auto &i:all2){
+        cout << i << ' ';
+    }
+    cout << endl;
+  }
+}
