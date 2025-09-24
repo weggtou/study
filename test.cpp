9a09a149d7227375e046b4cc44e6cfcbb7a96841
weggt <3044129132@qq.com>
2025-09-24 22:05:54 +0800

    1

--- test.cpp
+++ test.cpp
@@ -1,42 +1,42 @@
 #include <bits/stdc++.h>
-using namespace std;    
-class bit{
-  using u64 = unsigned long long;
-  u64 v = 0;
- public:
-  bit (u64 a):v(a){}
-  bit (const string& str){
-    v = 0;
-    for (auto i:str){v = (v<<1)|(i-'0');}
-  }
-  bit(const initializer_list<int>& indexes) {
-    for(auto &i:indexes){
-      v<<=1;
-      v|=i;
-    }
-  }
+using namespace std;
+int  test(const int &b){
   
-  bit& operator +=(u64 a){v+=a;return *this;}
-  bit& operator -=(u64 a){v-=a;return *this;}
-  bit& operator *=(u64 a){v*=a;return *this;}
-  bit& operator /=(u64 a){v/=a;return *this;}
-  bit& operator|=(u64 x) { return v |= x, *this; }
-  bit& operator^=(u64 x) { return v ^= x, *this; }
-  bit& operator<<=(u64 x) { return v <<= x, *this; }
-  bit& operator>>=(u64 x) { return v >>= x, *this; }
-  bit& operator++() { return ++v, *this; }
-  bit operator++(int) { return bit(v++); }
-  bit& operator--() { return --v, *this; }
-  bit operator--(int) { return bit(v--); }
-  friend istream& operator>> (istream& in,bit& x){return in >> x.v;}
-  friend ostream& operator<< (ostream& out,bit& x){return out << x.v;}
-};
-
+  return b;
+}
 int main(){
-  bit a = 100;
-  cout << a<<endl;
-  string b = "1010";
-  bit c={1,0,1};
-  cout << c;
-  cout <<a;
+  // class test{
+  //   int &a;
+  //   int b;
+  //   int c;
+  //  public:
+  //   test (int a,int b):a(a),b(b){}
+  //   void operator() ()const{
+  //     a= 1;
+  //   }
+  //   int re(){
+  //     return a;
+  //   }
+  // };
+  // int a;
+  // cin >> a;
+  // test x(a,2);
+  // x();
+  // cout << x.re();
+  class test{
+    int &a;
+    int b;
+   public:
+    test(int a,int b):a(a),b(b){}
+
+    void change () const {
+      a = 10;
+    }
+    int re(){
+      return a;
+    }
+  };
+  test a(10,11);
+  
+
 }
