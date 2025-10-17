e7f7c66db709d9c44ef7aa957415ed8de3bc9307
weggt <3044129132@qq.com>
2025-10-17 19:42:59 +0800

    1

--- test.cpp
+++ test.cpp
@@ -1,42 +1,9 @@
 #include <bits/stdc++.h>
+#include <ctype.h>
 using namespace std;
-int  test(const int &b){
-  
-  return b;
-}
-int main(){
-  // class test{
-  //   int &a;
-  //   int b;
-  //   int c;
-  //  public:
-  //   test (int a,int b):a(a),b(b){}
-  //   void operator() ()const{
-  //     a= 1;
-  //   }
-  //   int re(){
-  //     return a;
-  //   }
-  // };
-  // int a;
-  // cin >> a;
-  // test x(a,2);
-  // x();
-  // cout << x.re();
-  class test{
-    int &a;
-    int b;
-   public:
-    test(int a,int b):a(a),b(b){}
 
-    void change () const {
-      a = 10;
-    }
-    int re(){
-      return a;
-    }
-  };
-  test a(10,11);
-  
+int main(){
+  char s = 'a';
+  bool a = isupper(s);
 
 }
